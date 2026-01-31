547b7e20d4a944a6c354871dffe17f25e8bdcb41
urban-calderon <urbanocalderon@outlook.com>
2026-01-31 14:59:16 -0600

    feat: add new page task

--- app/pages/tasks.vue
+++ app/pages/tasks.vue
@@ -3,26 +3,36 @@
     <SharedHeader @click:create="openCreateModal" />
     
     <v-card class="rounded-lg" elevation="0" border>
-        <TasksTaskTable 
+        <TaskTable 
             :items="tasks?.data || []" 
             @click:edit="openEditModal"
-            @click:delete="handleDelete"
+            @click:delete="confirmDelete"
         />
     </v-card>
 
-    <TaskModal
+    <TaskModalForm
       v-model="isModalOpen"
       :task-to-edit="editingTask"
       @save="handleSaveTask"
+    />
+
+    <TaskModalDelete
+        v-model="isDeleteDialogOpen"
+        :task-title="taskToDelete?.title"
+        :loading="isDeleting"
+        @confirm="handleDeleteConfirm"
     />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Task } from '@/interfaces/task.interface';
-import TaskModal from '~/components/tasks/TaskModal.vue';
+import { useActionTask } from '@/composables/useActionTask';
+import TaskTable from '@/components/task/Table.vue';
+import TaskModalForm from '@/components/task/ModalForm.vue';
+import TaskModalDelete from '@/components/task/ModalDelete.vue';
 
-const { tasks, refresh, createTask, updateTask } = await useTask();
+const { tasks, refresh, createTask, updateTask, deleteTask } = await useActionTask();
 
 const isModalOpen = ref(false);
 const editingTask = ref<Task | null>(null);
@@ -36,6 +46,10 @@ const openEditModal = (task: Task) => {
   editingTask.value = task; 
   isModalOpen.value = true;
 };
+
+const isDeleteDialogOpen = ref(false);
+const taskToDelete = ref<Task | null>(null);
+const isDeleting = ref(false);
 
 const handleSaveTask = async (taskForm: any) => {
   try {
@@ -53,6 +67,24 @@ const handleSaveTask = async (taskForm: any) => {
   }
 };
 
-const handleDelete = async (task: Task) => {
-}
+const confirmDelete = (task: Task) => {
+  taskToDelete.value = task;
+  isDeleteDialogOpen.value = true;
+};
+
+const handleDeleteConfirm = async () => {
+  if (!taskToDelete.value) return;
+
+  isDeleting.value = true;
+  try {
+    await deleteTask(taskToDelete.value.id);
+    await refresh();
+    isDeleteDialogOpen.value = false; // Cerramos el modal
+    taskToDelete.value = null;      // Limpiamos la selección
+  } catch (error) {
+    console.error('Error al eliminar:', error);
+  } finally {
+    isDeleting.value = false;
+  }
+};
 </script>
